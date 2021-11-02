3d515ebd1d2c38828d5c640d7829dad17adfe966
martijor000 <82613327+martijor000@users.noreply.github.com>
2021-11-02 14:42:40 -0700

    Created components for my page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
+import { BodyComponentComponent } from './body-component/body-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    ProductComponentComponent,
+    BodyComponentComponent
   ],
   imports: [
     BrowserModule
