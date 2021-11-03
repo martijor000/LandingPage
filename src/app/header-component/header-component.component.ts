f73d9f2468c42741733900480e540ef4fcd15861
martijor000 <82613327+martijor000@users.noreply.github.com>
2021-11-03 08:50:44 -0700

    Added more routing in header component

--- src/app/header-component/header-component.component.ts
+++ src/app/header-component/header-component.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-component.component.scss']
 })
 export class HeaderComponentComponent implements OnInit {
-
+  urls: any[] = [
+    {route: '', name: 'Home'},
+    {route: '/products', name: 'Products'},
+    {route: '/contact', name: 'Contact'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
